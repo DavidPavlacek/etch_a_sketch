e0c5d39547393f19d2bda529220f7602cdbea857
DavidPavlacek <dav.pavlacek@gmail.com>
2021-09-03 17:17:33 +0200

    slide bar that changes number of grid cells added

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const gridCells = document.getElementById("grid-cells");
+let slider = document.getElementById("slider")
+
+makeGrid(16,16)
 
 function makeGrid(rows, cols) {
     gridCells.style.setProperty('--grid-rows', rows);
     gridCells.style.setProperty('--grid-cols', cols);
     for ( c = 0; c < ( rows * cols); c++) {
         let cell = document.createElement("div");
-        gridCells.appendChild(cell).className = "grid-item"
+        gridCells.appendChild(cell).className = "grid-item";
     }
 }
 
-makeGrid(16,16)
+function removeAllCells() {
+
+    while (gridCells.firstChild) {
+        gridCells.removeChild(gridCells.lastChild)
+    }
+}
+
+slider.addEventListener("mouseup", function() {
+    let sliderVal = this.value;
+    sliderVal = parseInt(sliderVal);
+    console.log(sliderVal);
+    removeAllCells();
+    return makeGrid(sliderVal, sliderVal);
+    
+})
