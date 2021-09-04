98342792acbc22f9a1e0b1b77772e0ad4b3534b4
DavidPavlacek <dav.pavlacek@gmail.com>
2021-09-04 18:20:26 +0200

    black pen working, unless you change a grid size

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const gridCells = document.getElementById("grid-cells");
-let slider = document.getElementById("slider")
-let gridSize = document.getElementById("grid-size")
+const slider = document.getElementById("slider");
+const gridSize = document.getElementById("grid-size");
+const clearBtn = document.getElementById("clear-btn");
+const blackBtn = document.getElementById("black-btn");
 
+let blackBtnIsActive = false;
 
+makeGrid(16,16)
+makeNewGrid();
+
+const gridItemList = document.querySelectorAll(".grid-item");
 
 function makeGrid(rows, cols) {
     gridCells.style.setProperty('--grid-rows', rows);
@@ -36,8 +43,20 @@ function makeNewGrid() {
     })
 }
 
+function pickColor() {
+    if (blackBtnIsActive == true) {
+        this.classList.add("black-pen")
+    }
+}
+
+function blackBtnClick() {
+    if (blackBtnIsActive == false) {
+        blackBtnIsActive = true;
+        gridItemList.forEach(cell => cell.addEventListener("mouseover", pickColor));
+    } else blackBtnIsActive = false
+    console.log(blackBtnIsActive);
+}
+
+blackBtn.addEventListener("click", blackBtnClick)
 
 
-
-makeGrid(16,16)
-makeNewGrid();
