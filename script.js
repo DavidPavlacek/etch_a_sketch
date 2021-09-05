d00c50a0a3a002dc513b3f27d703539ba4ce4a51
DavidPavlacek <dav.pavlacek@gmail.com>
2021-09-05 04:39:16 +0200

    clear button, event listener for all butons and better script style implemented

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gridCells = document.getElementById("grid-cells");
 const slider = document.getElementById("slider");
 const gridSize = document.getElementById("grid-size");
-const clearBtn = document.getElementById("clear-btn");
-const blackBtn = document.getElementById("black-btn");
+const allButtons = document.getElementById("color-selection-buttons");
 let gridItemList = document.getElementsByClassName('grid-item');
-let blackBtnIsActive = false;
 
-makeGrid(16,16)
-makeNewGrid();
+let blackBtnIsActive = false;
+let rainbowBtnIsActive = false;
+let scaleBtnIsActive = false;
+let pickBtnIsActive = false;
 
 function makeGrid(rows, cols) {   
     gridCells.style.setProperty('--grid-rows', rows);
@@ -15,12 +15,6 @@ function makeGrid(rows, cols) {
     for ( c = 0; c < ( rows * cols); c++) {
         let cell = document.createElement("div");
         gridCells.appendChild(cell).className = "grid-item";
-    }
-}
-
-function removeAllCells() {
-    while (gridCells.firstChild) {
-        gridCells.removeChild(gridCells.lastChild)
     }
 }
 
@@ -43,6 +37,44 @@ function makeNewGrid() {
     })  
 }
 
+allButtons.addEventListener("click", function(e) {
+
+    if (e.target && e.target.id == "black-btn") {
+        blackBtnClick();
+    }
+    if (e.target && e.target.id == "rainbow-btn") {
+        console.log(e.target.id + " was clicked");
+    }
+    if (e.target && e.target.id == "scale-btn") {
+        console.log(e.target.id + " was clicked");
+    }
+    if (e.target && e.target.id == "pick-btn") {
+        console.log(e.target.id + " was clicked");
+    }
+    if (e.target && e.target.id == "clear-btn") {
+        clearBtnClick();
+    }
+})
+
+function removeAllCells() {
+    while (gridCells.firstChild) {
+        gridCells.removeChild(gridCells.lastChild)
+    }
+}
+
+function disableAllButtons() {
+    blackBtnIsActive = false;
+    rainbowBtnIsActive = false;
+    scaleBtnIsActive = false;
+    pickBtnIsActive = false;
+}
+
+function clearBtnClick() {
+    disableAllButtons();
+    removeAllCells();
+    makeGrid(slider.value, slider.value)
+}
+
 function pickColor() {
     if (blackBtnIsActive == true) {
         this.classList.add("black-pen")
@@ -56,7 +88,5 @@ function blackBtnClick() {
     } else blackBtnIsActive = false
 }
 
-
-blackBtn.addEventListener("click", blackBtnClick)
-
-
+makeGrid(16,16);
+makeNewGrid();
